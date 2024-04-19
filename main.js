43fedaceede17b78fa52fb4afb89f341c5365686
Zoey <153301664+SalenskiZo375@users.noreply.github.com>
2024-04-19 10:33:35 -0400

    s

---
+++ main.js
@@ -0,0 +1,8 @@
+const firstName = 'Zoey Salenski';
+const date = '19 APR 2024';
+const projectName = 'JS SImple CaAlamlater'
+
+
+
+const numb1 = Num (prompt("please enter your number"));
+const numb2 = Num (prompt("please enter your other number"));
