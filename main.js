d785808ce63913db6e9087435aaf500e8857d984
Zoey <153301664+SalenskiZo375@users.noreply.github.com>
2024-04-19 10:53:33 -0400

    cerf

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-const firstName = 'Zoey Salenski';
-const date = '19 APR 2024';
-const projectName = 'JS SImple CaAlamlater'
+console.log ('Zoey Salenski');
+console.log ('19 APR 2024');
+console.log ('JS SImple CaAlamlater');
 
+console.log ('output:')
 
+const numb1 = (prompt("please enter your number"));
+const numb2 = (prompt("please enter your other number"));
 
-const numb1 = Num (prompt("please enter your number"));
-const numb2 = Num (prompt("please enter your other number"));
+const total1 = numb1 + numb2;
+const total2 = numb1 - numb2;
+const total3 = numb1 * numb2;
+const total4 = numb1 / numb2;
+
+console.log(`${numb1} + ${numb2} = ${total1}`);
+console.log(`${numb1} - ${numb2} = ${total2}`);
+console.log(`${numb1} * ${numb2} = ${total3}`);
+console.log(`${numb1} / ${numb2} = ${total4}`);
